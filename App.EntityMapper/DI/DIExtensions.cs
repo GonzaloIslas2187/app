590da24e5e61eec2edbcaf23be88ce3eae0738de
GonzaloIslas <44009322+GonzaloIslas@users.noreply.github.com>
2024-04-29 11:06:57 -0300

    initial commit

---
+++ App.EntityMapper/DI/DIExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace App.EntityMapper.DI;
+public static class DIExtensions
+{
+    public static IServiceCollection ConfigureAutoMapper(this IServiceCollection services)
+    {
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        return services;
+    }
+}
---
+++ App.EntityMapper/Profiles/DTOProfile.cs
@@ -0,0 +1,12 @@
+﻿using App.Domain.Operations;
+using App.DTOs;
+using AutoMapper;
+
+namespace App.EntityMapper.Profiles;
+public class DTOProfile : Profile
+{
+    public DTOProfile()
+    {
+        CreateMap<Operation, OperationDto>().ReverseMap();
+    }
+}
